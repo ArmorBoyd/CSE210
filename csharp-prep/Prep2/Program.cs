781cc60f3d510243e14db9cff0854541e065d2e4
ArmorBoyd <boydpoblete@gmail.com>
2023-09-12 16:52:54 +0800

    Prep 2, Done.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! you passed! Great Job!");
+        }
+        else
+        {
+            Console.WriteLine("Oh no! Unfortunately, you did not pass.");
+        }
     }
 }
+
+/*Console.Write("What is your percentage? ");
+        string percentage = Console.ReadLine();
+                
+        int percent = int.Parse(percentage);
+
+        if (percent >= 90)
+        {
+            Console.Write("Congratulations! You got an A. You passed with flying colors");
+        }
+        else if (percent >= 80)
+        {
+            Console.Write("Congratulations! You got a B. Great Job!");
+        }
+        else if (percent >= 70)
+        {
+            Console.Write("Congratulations! You got a C. You barely made it son.");
+        }
+        else if (percent >= 60)
+        {
+            Console.Write("Oh no! You got a D. Unfortunately, you did not pass. Please study more efficiently");
+        }
+        else
+        {
+            Console.Write("Oh no! You got an F. Try again next semester. Please study more efficiently");
+        }
+*/ 
