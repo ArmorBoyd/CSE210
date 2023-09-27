7011f95276f68f7e741354d57d7de8c5db5c4a12
ArmorBoyd <boydpoblete@gmail.com>
2023-09-27 16:45:27 +0800

    practice

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,8 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-    
-         Console.Write("What is the magic number? ");
+Console.Write("What is the magic number? ");
         int magicNum = int.Parse(Console.ReadLine());
         int guess = -1;
 
@@ -28,6 +27,34 @@ class Program
                 Console.WriteLine("You guessed it!");
             }
 
-        }                    
+        }
     }
 }
+
+  
+
+         //Q&A with lifeline
+          /*int lives = 5;
+          string lucky = "69";
+          string answer;
+
+        while (lives > 0)
+        {
+            Console.WriteLine("Life: " + lives);
+            Console.Write("What is the lucky number? ");
+            Console.WriteLine();
+            Console.Write("The lucky number is: ");
+            answer = Console.ReadLine();
+
+            if (answer.Equals(lucky))
+            {
+                Console.Write("Congrats");
+                break;
+            }
+            else lives--;
+            
+            if (lives == 0) Console.Write("Idiot");
+            Console.WriteLine();
+            Console.WriteLine();
+
+        } */
