8a381b9c0bda4c1a44412e2cca2cab2ee973847b
ArmorBoyd <boydpoblete@gmail.com>
2023-10-23 21:58:49 +0800

    Week 06

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,41 @@
 using System;
 
-class Program
+namespace DailyScripture
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop03 World!");
+        static void Main()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter the scripture reference:");
+            string reference = Console.ReadLine();
+
+            Console.WriteLine("Enter the scripture passage:");
+            string passage = Console.ReadLine();
+
+            Reference scriptureReference = new Reference(reference, 1, 1);
+            Scripture scripture = new Scripture(scriptureReference, passage);
+
+            Console.Clear();
+            scripture.Display();
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+
+                if (!scripture.HideRandomWord())
+                {
+                    Console.WriteLine("\nYou got this, you memorized the scripture!!");
+                    break;
+                }
+
+                Console.Clear();
+                scripture.Display();
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+            }
+        }
     }
 }
