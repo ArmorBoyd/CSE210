55b06a147a83611d154b1085fcf9ad7d86ebe4d7
ArmorBoyd <boydpoblete@gmail.com>
2023-11-27 14:12:35 +0800

    Week 12

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video video1 = new Video("Title 1", "Author 1", 120);
+        Video video2 = new Video("Title 2", "Author 2", 180);
+
+        video1.AddComment(new Comment("User1", "Nice video!"));
+        video1.AddComment(new Comment("User2", "Great content!"));
+        video2.AddComment(new Comment("User3", "Interesting topic!"));
+        video2.AddComment(new Comment("User4", "I enjoyed watching this."));
+
+        List<Video> videos = new List<Video> { video1, video2 };
+
+        foreach (var video in videos)
+        {
+            Console.WriteLine($"Title: {video.GetTitle()}");
+            Console.WriteLine($"Author: {video.GetAuthor()}");
+            Console.WriteLine($"Length (seconds): {video.GetLengthInSeconds()}");
+            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
+
+            foreach (var comment in video.GetComments())
+            {
+                Console.WriteLine($"Comment by {comment.GetCommenterName()}: {comment.GetCommentText()}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
