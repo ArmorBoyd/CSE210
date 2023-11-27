126a1c5fa60deda77c45545db25236aec351c7f8
ArmorBoyd <boydpoblete@gmail.com>
2023-11-27 18:04:01 +0800

    Week 12

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -5,16 +5,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Video video1 = new Video("Title 1", "Author 1", 120);
-        Video video2 = new Video("Title 2", "Author 2", 180);
+        // Creating videos
+        Video video1 = new Video("Upgrade your setup for Free! 10 tips and tricks to improve your Setup!", "TechSource", 780);
+        Video video2 = new Video("Setup Wars - Episode 326", "TechSource", 840);
+        Video video3 = new Video("Setup Wars Episode 310 - Budget Edition", "TechSource", 501);
 
-        video1.AddComment(new Comment("User1", "Nice video!"));
-        video1.AddComment(new Comment("User2", "Great content!"));
-        video2.AddComment(new Comment("User3", "Interesting topic!"));
-        video2.AddComment(new Comment("User4", "I enjoyed watching this."));
+        // Adding comments to videos
+        video1.AddComment(new Comment("Ernie Ritz", "Nice video!"));
+        video1.AddComment(new Comment("Sild Gwyn", "Great content!"));
+        video1.AddComment(new Comment("Sheng Weng", "Awesome!"));
 
-        List<Video> videos = new List<Video> { video1, video2 };
+        video2.AddComment(new Comment("Jim Russ", "Interesting Setup!"));
+        video2.AddComment(new Comment("Shelly Pot", "I enjoyed watching this."));
+        video2.AddComment(new Comment("Gly Gly", "Keep up the good work!"));
 
+        video3.AddComment(new Comment("Wy Net", "Very informative."));
+        video3.AddComment(new Comment("Fritz Delics", "Loved it!"));
+        video3.AddComment(new Comment("Ja Ninsky", "Looking forward to more budget edition."));
+
+        // Putting videos in a list
+        List<Video> videos = new List<Video> { video1, video2, video3 };
+
+        // Displaying video information and comments
         foreach (var video in videos)
         {
             Console.WriteLine($"Title: {video.GetTitle()}");
---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+class Order
+{
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(Customer customer)
+    {
+        this.customer = customer;
+        products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public decimal CalculateTotalPrice()
+    {
+        decimal totalPrice = 0;
+        foreach (var product in products)
+        {
+            totalPrice += product.GetTotalPrice();
+        }
+
+        // Shipping cost calculation
+        decimal shippingCost = customer.IsInUSA() ? 5 : 35;
+        totalPrice += shippingCost;
+
+        return totalPrice;
+    }
+
+    public string GetPackingLabel()
+    {
+        string packingLabel = "Packing Label:\n";
+        foreach (var product in products)
+        {
+            packingLabel += $"{product.GetName()} - {product.GetProductId()}\n";
+        }
+        return packingLabel;
+    }
+
+    public string GetShippingLabel()
+    {
+        string shippingLabel = "Shipping Label:\n";
+        shippingLabel += $"Customer: {customer.GetName()}\n";
+        shippingLabel += $"Address:\n{customer.GetAddress().GetFullAddress()}";
+        return shippingLabel;
+    }
+}
