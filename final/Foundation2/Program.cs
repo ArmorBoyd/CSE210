88a2b278d9489d783ca46777f6cadbd44a256e1b
ArmorBoyd <boydpoblete@gmail.com>
2023-11-27 19:25:03 +0800

    Week 12

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,33 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Creating addresses
+        // Create addresses
         Address address1 = new Address("123 Main St", "Anytown", "CA", "USA");
         Address address2 = new Address("456 Elm St", "Othertown", "NY", "Canada");
 
-        // Creating customers
+        // Create customers
         Customer customer1 = new Customer("John Doe", address1);
         Customer customer2 = new Customer("Jane Smith", address2);
 
-        // Creating products
+        // Create products
         Product product1 = new Product("Product 1", "P001", 10.5m, 2);
         Product product2 = new Product("Product 2", "P002", 15m, 3);
+        Product product3 = new Product("Product 3", "P003", 8m, 1);
 
-        // Creating orders
+        // Create orders
         Order order1 = new Order(customer1);
-        Order order2 = new Order(customer2);
-
         order1.AddProduct(product1);
         order1.AddProduct(product2);
-        order2.AddProduct(product2);
 
-        // Displaying results for order 1
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product2);
+        order2.AddProduct(product3);
+
+        // Display results for order 1
         Console.WriteLine("Order 1:");
         Console.WriteLine(order1.GetPackingLabel());
         Console.WriteLine(order1.GetShippingLabel());
         Console.WriteLine($"Total Price: ${order1.CalculateTotalPrice()}");
 
-        // Displaying results for order 2
+        // Display results for order 2
         Console.WriteLine("\nOrder 2:");
         Console.WriteLine(order2.GetPackingLabel());
         Console.WriteLine(order2.GetShippingLabel());
